109e7fc4c45d1ed261a6c52b64ab7f57f431393a
Hrvoje SInovec <hsinovec@tvz.hr>
2025-01-17 22:35:17 +0100

    Initial commit

---
+++ src/main/java/com/tvz/utrke/UtrkeApplication.java
@@ -0,0 +1,13 @@
+package com.tvz.utrke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtrkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtrkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tvz/utrke/UtrkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvz.utrke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtrkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
