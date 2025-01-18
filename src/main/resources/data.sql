b6d90b1f90e8357e5d6e72c2001f693d240a1597
Hrvoje SInovec <hsinovec@tvz.hr>
2025-01-18 12:12:00 +0100

    H2 i OpenAI token

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE TEMPLATE (
+                          ID INT,
+                          NAME VARCHAR(50)
+);
+
+INSERT INTO TEMPLATE (ID, NAME) VALUES (0, 'TEST');
+INSERT INTO TEMPLATE (ID, NAME) VALUES (1, 'TEST');
